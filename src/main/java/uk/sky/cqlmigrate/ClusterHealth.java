97cc80be5a718ac5e9159d954495074fe50fb426
Adam Dougal <adam.dougal@sky.uk>
2017-12-21 11:33:28 +0000

    Fixes race condition in test where host status is not updated quick enough
    Fixes #69

--- src/main/java/uk/sky/cqlmigrate/ClusterHealth.java
+++ src/main/java/uk/sky/cqlmigrate/ClusterHealth.java
@@ -2,13 +2,18 @@ package uk.sky.cqlmigrate;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.sky.cqlmigrate.exception.ClusterUnhealthyException;
 
 import java.net.InetAddress;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 class ClusterHealth {
+
+    private static final Logger log = LoggerFactory.getLogger(ClusterHealth.class);
 
     private final Cluster cluster;
 
@@ -18,7 +23,11 @@ class ClusterHealth {
 
     void check() throws ClusterUnhealthyException {
 
-        List<InetAddress> unhealthyHosts = cluster.getMetadata().getAllHosts()
+        log.debug("Checking cluster health");
+
+        Set<Host> allHosts = cluster.getMetadata().getAllHosts();
+
+        List<InetAddress> unhealthyHosts = allHosts
                 .stream()
                 .filter(host -> !host.isUp())
                 .map(Host::getAddress)
@@ -27,5 +36,7 @@ class ClusterHealth {
         if (!unhealthyHosts.isEmpty()) {
             throw new ClusterUnhealthyException("Cluster not healthy, the following hosts are down: " + unhealthyHosts);
         }
+
+        log.debug("All hosts healthy: {}", allHosts);
     }
 }
