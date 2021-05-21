057cb733e4311249bf7c096bfdc5ad70880f9b1c
David Heath <david.heath@sky.uk>
2021-05-21 09:34:36 +0100

    Check for previous files being applied in pre-migration checker

--- src/main/java/uk/sky/cqlmigrate/PreMigrationChecker.java
+++ src/main/java/uk/sky/cqlmigrate/PreMigrationChecker.java
@@ -1,19 +1,31 @@
 package uk.sky.cqlmigrate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import static uk.sky.cqlmigrate.SchemaUpdates.SCHEMA_UPDATES_TABLE;
 
 public class PreMigrationChecker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreMigrationChecker.class);
 
     private final SessionContext sessionContext;
     private final String keyspace;
+    private final SchemaChecker schemaChecker;
+    private final CqlPaths paths;
 
-    public PreMigrationChecker(SessionContext sessionContext, String keyspace) {
+    public PreMigrationChecker(SessionContext sessionContext, String keyspace, SchemaChecker schemaChecker, CqlPaths paths) {
         this.sessionContext = sessionContext;
         this.keyspace = keyspace;
+        this.schemaChecker = schemaChecker;
+        this.paths = paths;
     }
 
     boolean migrationIsNeeded() {
-        return !keyspaceExists() || !schemaUpdatesTableExists();
+        return !keyspaceExists() || !schemaUpdatesTableExists() || !allMigrationFilesApplied();
     }
 
     private boolean keyspaceExists() {
@@ -24,5 +36,22 @@ public class PreMigrationChecker {
         return sessionContext.getSession().getCluster().getMetadata().getKeyspace(keyspace).getTable(SCHEMA_UPDATES_TABLE) != null;
     }
 
+    private boolean allMigrationFilesApplied() {
+        List<String> filesNotApplied = new ArrayList<>();
+        paths.applyInSortedOrder((filename, path) -> {
+            if (schemaChecker.alreadyApplied(filename)) {
+                if (schemaChecker.contentsAreDifferent(filename, path)) {
+                    LOGGER.error("Contents have changed: {}", path.getFileName());
+                    throw new IllegalStateException("Contents have changed for " + filename + " at " + path);
+                } else {
+                    LOGGER.info("Already applied: {}, skipping", path.getFileName());
+                }
+            } else {
+                filesNotApplied.add(filename);
+            }
+        });
 
+        LOGGER.info("Found {} files to be applied", filesNotApplied.size());
+        return filesNotApplied.isEmpty();
+    }
 }
--- src/test/java/uk/sky/cqlmigrate/PreMigrationCheckerIntegrationTest.java
+++ src/test/java/uk/sky/cqlmigrate/PreMigrationCheckerIntegrationTest.java
@@ -1,10 +1,7 @@
 package uk.sky.cqlmigrate;
 
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ConsistencyLevel;
-import com.datastax.driver.core.KeyspaceMetadata;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.*;
 import com.google.common.util.concurrent.Uninterruptibles;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.thrift.transport.TTransportException;
@@ -12,7 +9,19 @@ import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import org.junit.*;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,13 +54,19 @@ public class PreMigrationCheckerIntegrationTest {
     }
 
     @Test
-    public void migrationNotNeededIfEverythingExists() {
+    public void migrationNotNeededIfEverythingExists() throws Exception {
         //given
         cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
         cluster.connect(TEST_KEYSPACE).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+
+        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:56-create-status-table.cql', 'fa03a30eab18b64b74ee1ea7816e0513f03b4ac7', dateof(now()));");
+        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:57-add-column-to-status-table.cql', '93abe7ec3b14888b9a8bd8d680a30839a92a3248', dateof(now()));");
+        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:59-add-reference-data-to-status-table.cql', '75e456f43dd5e1b099091319bbf6bf047d8bc34b', dateof(now()));");
         Session session = cluster.connect(TEST_KEYSPACE);
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
-        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE);
+        SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
+
+        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE, schemaChecker, CqlPaths.create(Collections.singletonList(getResourcePath("cql_valid_one"))));
 
         //when
         boolean migrationIsNeeded = preMigrationChecker.migrationIsNeeded();
@@ -66,7 +81,8 @@ public class PreMigrationCheckerIntegrationTest {
         cluster.connect("system").execute("DROP KEYSPACE IF EXISTS " + TEST_KEYSPACE + ";");
         Session session = cluster.connect("system");
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
-        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE);
+        SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
+        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE, schemaChecker, CqlPaths.create(Collections.emptyList()));
 
         //when
         boolean migrationIsNeeded = preMigrationChecker.migrationIsNeeded();
@@ -81,13 +97,60 @@ public class PreMigrationCheckerIntegrationTest {
         cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
         Session session = cluster.connect("system");
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
-        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE);
+        SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
+        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE, schemaChecker, CqlPaths.create(Collections.emptyList()));
 
         //when
         boolean migrationIsNeeded = preMigrationChecker.migrationIsNeeded();
 
         //then
         assertThat(migrationIsNeeded).isTrue();
+    }
+
+    @Test
+    public void shouldMigrateIfPreFlightChecksEnabledAndNoChangesApplied() throws Exception {
+        //given
+        Collection<Path> cqlPaths = new ArrayList<>();
+        cqlPaths.add(getResourcePath("cql_valid_one"));
+
+        cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
+        cluster.connect(TEST_KEYSPACE).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+        Session session = cluster.connect("system");
+        SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
+        SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
+
+        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE, schemaChecker, CqlPaths.create(cqlPaths));
+
+        //when
+        boolean migrationIsNeeded = preMigrationChecker.migrationIsNeeded();
+
+        //then
+        assertThat(migrationIsNeeded).isTrue();
+    }
+
+    @Test
+    public void shouldMigrateIfPreFlightChecksEnabledAndSomeChangesNotApplied() throws Exception {
+        //given
+        cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
+        cluster.connect(TEST_KEYSPACE).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+
+        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:56-create-status-table.cql', 'fa03a30eab18b64b74ee1ea7816e0513f03b4ac7', dateof(now()));");
+        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:57-add-column-to-status-table.cql', '93abe7ec3b14888b9a8bd8d680a30839a92a3248', dateof(now()));");
+        Session session = cluster.connect("system");
+        SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
+        SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
+
+        PreMigrationChecker preMigrationChecker = new PreMigrationChecker(sessionContext, TEST_KEYSPACE, schemaChecker, CqlPaths.create(Collections.singletonList(getResourcePath("cql_valid_one"))));
+
+        //when
+        boolean migrationIsNeeded = preMigrationChecker.migrationIsNeeded();
+
+        //then
+        assertThat(migrationIsNeeded).isTrue();
+    }
+
+    private Path getResourcePath(String resourcePath) throws URISyntaxException {
+        return Paths.get(ClassLoader.getSystemResource(resourcePath).toURI());
     }
 
 }
