91a7ae232ab475f043bd8955b7bbee271c7a7c3f
David Heath <david.heath@sky.uk>
2021-06-02 08:32:24 +0100

    Reduce deuplication in tests

--- src/test/java/uk/sky/cqlmigrate/PreMigrationCheckerIntegrationTest.java
+++ src/test/java/uk/sky/cqlmigrate/PreMigrationCheckerIntegrationTest.java
@@ -5,23 +5,19 @@ import com.datastax.driver.core.*;
 import com.google.common.util.concurrent.Uninterruptibles;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.thrift.transport.TTransportException;
+import org.assertj.core.groups.Tuple;
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import org.junit.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,6 +28,8 @@ public class PreMigrationCheckerIntegrationTest {
     private static Cluster cluster;
     private static Session session;
     private static ClusterHealth clusterHealth;
+
+    private static List<Tuple> allSchemaUpdates = new ArrayList<>();
 
     @BeforeClass
     public static void setupCassandra() throws ConfigurationException, IOException, TTransportException, InterruptedException {
@@ -45,6 +43,10 @@ public class PreMigrationCheckerIntegrationTest {
     @Before
     public void setUp() throws Exception {
         session.execute("DROP KEYSPACE IF EXISTS cqlmigrate_test");
+        allSchemaUpdates.clear();
+        allSchemaUpdates.add(new Tuple("2015-04-01-13:56-create-status-table.cql", "fa03a30eab18b64b74ee1ea7816e0513f03b4ac7"));
+        allSchemaUpdates.add(new Tuple("2015-04-01-13:57-add-column-to-status-table.cql", "93abe7ec3b14888b9a8bd8d680a30839a92a3248"));
+        allSchemaUpdates.add(new Tuple("2015-04-01-13:59-add-reference-data-to-status-table.cql", "75e456f43dd5e1b099091319bbf6bf047d8bc34b"));
     }
 
     @AfterClass
@@ -56,12 +58,10 @@ public class PreMigrationCheckerIntegrationTest {
     @Test
     public void migrationNotNeededIfEverythingExists() throws Exception {
         //given
-        cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
-        cluster.connect(TEST_KEYSPACE).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+        createKeyspace(TEST_KEYSPACE);
+        createSchemaUpdatesTable(TEST_KEYSPACE);
+        allSchemaUpdates.forEach(t -> insertSchemaUpdate(TEST_KEYSPACE, t));
 
-        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:56-create-status-table.cql', 'fa03a30eab18b64b74ee1ea7816e0513f03b4ac7', dateof(now()));");
-        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:57-add-column-to-status-table.cql', '93abe7ec3b14888b9a8bd8d680a30839a92a3248', dateof(now()));");
-        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:59-add-reference-data-to-status-table.cql', '75e456f43dd5e1b099091319bbf6bf047d8bc34b', dateof(now()));");
         Session session = cluster.connect(TEST_KEYSPACE);
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
         SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
@@ -78,7 +78,6 @@ public class PreMigrationCheckerIntegrationTest {
     @Test
     public void migrationNeededIfKeyspaceDoesNotExist() {
         //given
-        cluster.connect("system").execute("DROP KEYSPACE IF EXISTS " + TEST_KEYSPACE + ";");
         Session session = cluster.connect("system");
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
         SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
@@ -94,7 +93,7 @@ public class PreMigrationCheckerIntegrationTest {
     @Test
     public void migrationNeededIfSchemaUpdatesTableDoesNotExist() {
         //given
-        cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
+        createKeyspace(TEST_KEYSPACE);
         Session session = cluster.connect("system");
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
         SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
@@ -113,8 +112,8 @@ public class PreMigrationCheckerIntegrationTest {
         Collection<Path> cqlPaths = new ArrayList<>();
         cqlPaths.add(getResourcePath("cql_valid_one"));
 
-        cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
-        cluster.connect(TEST_KEYSPACE).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+        createKeyspace(TEST_KEYSPACE);
+        createSchemaUpdatesTable(TEST_KEYSPACE);
         Session session = cluster.connect("system");
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
         SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
@@ -131,11 +130,10 @@ public class PreMigrationCheckerIntegrationTest {
     @Test
     public void shouldMigrateIfPreFlightChecksEnabledAndSomeChangesNotApplied() throws Exception {
         //given
-        cluster.connect("system").execute("CREATE KEYSPACE " + TEST_KEYSPACE + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
-        cluster.connect(TEST_KEYSPACE).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+        createKeyspace(TEST_KEYSPACE);
+        createSchemaUpdatesTable(TEST_KEYSPACE);
+        allSchemaUpdates.subList(0, 2).forEach(t -> insertSchemaUpdate(TEST_KEYSPACE, t));
 
-        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:56-create-status-table.cql', 'fa03a30eab18b64b74ee1ea7816e0513f03b4ac7', dateof(now()));");
-        cluster.connect(TEST_KEYSPACE).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('2015-04-01-13:57-add-column-to-status-table.cql', '93abe7ec3b14888b9a8bd8d680a30839a92a3248', dateof(now()));");
         Session session = cluster.connect("system");
         SessionContext sessionContext = new SessionContext(session, ConsistencyLevel.ALL, ConsistencyLevel.ALL, clusterHealth);
         SchemaChecker schemaChecker = new SchemaChecker(sessionContext, TEST_KEYSPACE);
@@ -151,6 +149,18 @@ public class PreMigrationCheckerIntegrationTest {
 
     private Path getResourcePath(String resourcePath) throws URISyntaxException {
         return Paths.get(ClassLoader.getSystemResource(resourcePath).toURI());
+    }
+
+    private void createKeyspace(String keyspaceName) {
+        cluster.connect("system").execute("CREATE KEYSPACE " + keyspaceName + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };");
+    }
+
+    private void createSchemaUpdatesTable(String keyspaceName) {
+        cluster.connect(keyspaceName).execute("CREATE TABLE schema_updates (filename text primary key, checksum text, applied_on timestamp);");
+    }
+
+    private void insertSchemaUpdate(String keyspaceName, Tuple filenameAndChecksum) {
+        cluster.connect(keyspaceName).execute("INSERT INTO schema_updates (filename, checksum, applied_on) VALUES ('" + filenameAndChecksum.toArray()[0] + "', '" + filenameAndChecksum.toArray()[1] + "', dateof(now()));");
     }
 
 }
